eab47504152612f152ef2a4b5229823a8ec3d9db
Greg McClure <gmmcclure@gmail.com>
2023-09-29 14:16:45 +0100

    First commit

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
-    print("Hello, World!")
+    float_variable = 5.0
+    integer_variable = 55
+
+    print(f'the sum of {float_variable} and {integer_variable} is: {float_variable + integer_variable}')
+    print(f'the sum of {float_variable} and {integer_variable} '
+          f'converted to integer is: {int(float_variable + integer_variable)}')
 
 
 if __name__ == '__main__':
     main()
 
-# Hello, World!
+# the sum of 5.0 and 55 is: 60.0
+# the sum of 5.0 and 55 converted to integer is: 60
+
